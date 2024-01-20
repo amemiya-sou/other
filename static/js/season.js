b9ff2f6385400f58ab987490e40419dea041f795
amemiya-sou <108377846+amemiya-sou@users.noreply.github.com>
2024-01-21 07:37:28 +0900

    Add files via upload

--- static/js/season.js
+++ static/js/season.js
@@ -1,5 +1,5 @@
 const selectElement = document.getElementById("season");
-for (let i = 13; i >= 1; i--) {
+for (let i = 14; i >= 1; i--) {
     const option = document.createElement("option");
     option.value = `season${i}`;
     option.text = `シーズン${i}`;
